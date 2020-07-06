42925bc71096f51c13a7131a2bcb807313002ead
hpneo <hpneo@hotmail.com>
2020-07-05 19:23:44 -0500

    Add cache

--- app.rb
+++ app.rb
@@ -1,12 +1,30 @@
 require 'sinatra'
+require 'rack/deflater'
+require 'rack/cache'
 require 'json'
 require_relative './ubigeo'
 require_relative './directorio'
 
 class Application < Sinatra::Base
   set :protection, :except => :json_csrf
+  use Rack::Deflater
+
+  if ENV['MEMCACHEDCLOUD_SERVERS']
+    dalli = Dalli::Client.new(
+      ENV['MEMCACHEDCLOUD_SERVERS'].split(','),
+      username: ENV['MEMCACHEDCLOUD_USERNAME'],
+      password: ENV['MEMCACHEDCLOUD_PASSWORD']
+    )
+
+    use Rack::Cache,
+        verbose: true,
+        metastore: dalli,
+        entitystore: dalli
+  end
 
   before do
+    cache_control :public, :must_revalidate, max_age: 604800
+
     if request.request_method == 'OPTIONS'
       response.headers["Access-Control-Allow-Origin"] = "*"
       response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
